890712d5d8bf9a843f5adf049dab2d19eeccc57b
Ka-hels <145565585+Ka-hels@users.noreply.github.com>
2025-03-12 13:30:34 +0000

    Quiz Continued
    Continued work on the quizzes:
    
    Got some Javascript stuff set up but it needs finishing and testing

--- Script.js
+++ Script.js
@@ -21,6 +21,32 @@ function myFunction() {
 }
 
 function checkAnswer(){
+    var check = 0
+    var ans1 = document.getElementbyId("ans1")
+    var ans2 = document.getElementbyId("ans2")
+    var ans3 = document.getElementbyId("ans3")
+    if (ans1.value == ans1.className){
+        check += 1
+    }
+    if (ans2.value == ans2.className){
+        check += 1
+    }
+    if (ans3.value == ans3.className){
+        check += 1
+    }
+    if (check == 3){
+        correctAnswer()
+    } else{
+        wrongAnswer()
+    }
+}
+
+function correctAnswer(){
+    var x = document.getElementById("right")
+    x.style.display = float;
+}
+
+function wrongAnswer(){
     
 }
 
