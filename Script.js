1838ed7732be9db308afd3659433316a866236fc
Ka-hels <145565585+Ka-hels@users.noreply.github.com>
2025-03-12 11:33:45 +0000

    Responsive Navbars
    Both navbars now change to reflect the screen size.
    Topnav gets rid of all links except Home and a new Icon which opens a vertical menu for the links
    Sidenav disappears on small screens (internal navigation only so not necessary when screen size is limited)

--- Script.js
+++ Script.js
@@ -11,6 +11,16 @@ function adjustSidenavHeight()//Changes the sidenav height to avoid overlap with
     document.querySelector(".sidenav").style.marginBottom = `${botnavHeight}px`;
 }
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
 window.addEventListener('resize', adjustSidenavHeight);
 window.addEventListener('load', adjustSidenavHeight);
 window.addEventListener('scroll', adjustSidenavHeight);
+
